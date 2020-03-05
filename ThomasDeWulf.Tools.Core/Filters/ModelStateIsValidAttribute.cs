b8d0dcb859a1e6b0f087dc8d239775ae407a6970
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-03-05 14:22:59 +0100

    Add ModelStateIsValid attribute.

---
+++ ThomasDeWulf.Tools.Core/Filters/ModelStateIsValidAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ThomasDeWulf.Tools.Core.Helpers;
+
+namespace ThomasDeWulf.Tools.Core.Filters
+{
+    /// <summary>
+    /// Custom attribute that stops the request pipeline if the ModelState isn't valid. Returns a 400 Bad Request.
+    /// </summary>
+    public class ModelStateIsValidAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errorList = ModelStateFormatter.FormatErrors(context.ModelState);
+                context.Result = new BadRequestObjectResult(errorList);
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ ThomasDeWulf.Tools.Core/Helpers/ModelStateFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ThomasDeWulf.Tools.Core.Helpers
+{
+    public class ModelStateFormatter
+    {
+        public static IEnumerable<string> FormatErrors(ModelStateDictionary modelState)
+        {
+            return modelState.Values.SelectMany(v => v.Errors).Select(v => v.ErrorMessage + " " + v.Exception?.Message);
+        }
+    }
+}
